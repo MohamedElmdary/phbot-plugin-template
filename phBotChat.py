59d9f1fb0229048c4d2cd6db986858766e9791af
MohamedElmdary <engm5081@gmail.com>
2024-11-07 20:26:05 +0200

    feat: Add phBotChat types

---
+++ phBotChat.py
@@ -0,0 +1,10 @@
+def All(text: str) -> bool: pass
+def Party(text: str) -> bool: pass
+def Guild(text: str) -> bool: pass
+def Union(text: str) -> bool: pass
+def Stall(text: str) -> bool: pass
+def Private(name: str, text: str) -> bool: pass
+def Note(name: str, text: str) -> bool: pass
+def Global(text: str) -> bool: pass
+def Notice(text: str) -> bool: pass
+def ClientNotice(text: str) -> bool: pass
