46de00120d74d1160d51ec0cf110aae77e6c610a
MohamedElmdary <engm5081@gmail.com>
2024-11-07 20:26:47 +0200

    chore: add plugin example

---
+++ plugin/plugin.py
@@ -0,0 +1,18 @@
+from phBot import *
+
+# ______________________________@@ Init @@______________________________ #
+pName = 'template'
+pVersion = '1.0.0'
+
+# ______________________________@@ Config @@______________________________ #
+def load_config():
+    pass
+
+# ______________________________@@ Events @@______________________________ #
+def event_loop():
+    log('once every 0.5s')
+
+# ______________________________@@ Load @@______________________________ #
+log(f'plugin: {pName} v{pVersion} successfully loaded')
+
+load_config()
